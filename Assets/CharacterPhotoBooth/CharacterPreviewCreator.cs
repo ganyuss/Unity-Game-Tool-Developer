d29278f675d1f1878fe9edf4d3058923e82a11df
Clément Couture <clement.couture@beatfitnessgames.com>
2021-10-20 20:15:22 +0200

    Added character booth to create character previews

---
+++ Assets/CharacterPhotoBooth/CharacterPreviewCreator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CharacterPreviewCreator : MonoBehaviour {
+    [SerializeField]
+    private GameObject CharacterPreview;
+    [SerializeField]
+    private Transform CharacterParent;
+    [SerializeField]
+    private Camera RenderCamera;
+
+    public void CreatePreview(GameObject previewTargetPrefab, string targetFile) {
+        CharacterPreview.SetActive(false);
+        GameObject previewTarget = Instantiate(previewTargetPrefab, CharacterParent);
+        previewTarget.transform.position = Vector3.zero;
+
+        Texture2D outputTexture = RenderFrameToTexture();
+        AssetDatabase.CreateAsset(outputTexture, targetFile);
+
+        // For testing purposes, also write to a file in the project folder
+        File.WriteAllBytes(targetFile, outputTexture.EncodeToPNG());
+
+        DestroyImmediate(previewTarget);
+        CharacterPreview.SetActive(true);
+    }
+
+    // source: https://docs.unity3d.com/ScriptReference/Camera.Render.html
+    private Texture2D RenderFrameToTexture() {
+        // The Render Texture in RenderTexture.active is the one
+        // that will be read by ReadPixels.
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = RenderCamera.targetTexture;
+
+        // Render the camera's view.
+        RenderCamera.Render();
+
+        // Make a new texture and read the active Render Texture into it.
+        var targetTexture = RenderCamera.targetTexture;
+        Texture2D image = new Texture2D(targetTexture.width, targetTexture.height);
+        image.ReadPixels(new Rect(0, 0, targetTexture.width, targetTexture.height), 0, 0);
+        image.Apply();
+
+        // Replace the original active Render Texture.
+        RenderTexture.active = currentRT;
+
+        return image;
+    }
+}
---
+++ Assets/CharacterPhotoBooth/PreviewTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreviewTest : EditorWindow
+{
+    [MenuItem("Preview test/test")]
+    public static void ShowWindow() {
+        GetWindow<PreviewTest>().Show();
+    }
+
+    private const string SceneName = "Assets/CharacterPhotoBooth/CharacterPhotoShootBooth.unity";
+
+    private string targetFilePath = "Assets/";
+    private GameObject objectToPreview;
+    
+    private void OnEnable() {
+        
+    }
+
+    private void OnGUI() {
+
+        targetFilePath = EditorGUILayout.TextField("Target file path", targetFilePath);
+        objectToPreview = (GameObject)EditorGUILayout.ObjectField("Object to create preview of", objectToPreview, typeof(GameObject), false);
+        
+        if (GUILayout.Button("Create Preview")) {
+
+            Scene[] loadedScenes = GetAllLoadedScenes().ToArray();
+            
+            if (loadedScenes.Any(s => s.isDirty)) {
+                if (EditorUtility.DisplayDialog("Edited scenes found", "Do you want to save the opened scenes ?", "Yes",
+                    "No")) {
+                    EditorSceneManager.SaveScenes(loadedScenes);
+                }
+            }
+            
+            Scene shootBoothScene = EditorSceneManager.OpenScene(SceneName, OpenSceneMode.Single);
+            GetComponentInScene<CharacterPreviewCreator>(shootBoothScene).CreatePreview(objectToPreview, targetFilePath);
+        }
+    }
+
+    private T GetComponentInScene<T>(Scene scene, bool includeInactive = false) where T : UnityEngine.Object {
+        
+        foreach (var gameObject in scene.GetRootGameObjects()) {
+            T component = gameObject.GetComponentInChildren<T>(includeInactive);
+
+            if (component != null) {
+                return component;
+            }
+        }
+
+        return null;
+    }
+
+    private IEnumerable<Scene> GetAllLoadedScenes() {
+        for (int i = 0; i < SceneManager.sceneCount; i++) {
+            yield return SceneManager.GetSceneAt(i);
+        }
+    } 
+}
